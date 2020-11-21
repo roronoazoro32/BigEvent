f15493b8fdbc297ab6ff9f104b1fd481536be07a
Liao <2429463592@qq.com>
2020-11-21 11:33:30 +0800

    更新数据+重置

--- user/userInfo/userInfo.js
+++ user/userInfo/userInfo.js
@@ -8,25 +8,28 @@ var form = layui.form;
 //      发送ajax请求,获取用户信息
 //      设置表单各项(每个输入框)的value值
 
-$.ajax({
-    url: '/my/userinfo',
-    success: function (res) {
-        layer.msg(res.message)
-     if (res.status==0) {
-            console.log(res);
-        // 慢方法 ---  属性选择器
-        // $('input[name=username]').val(res.data.username);
-        // $('input[name=nickname]').val(res.data.nickname);
-        // $('input[name=email]').val(res.data.email);
-
-        // LayUI 内置快速填充的方法的要求：
-        // 先设置表单各项的 name属性（username/nickname/email/id）和接口的参数名对应
-        // 为form表单设置 `lay-filter="user"` ；值随便定义，我这里使用的是 user
-        // 调用方法
-        form.val('user',res.data)
-     }
-    }
-})
+function getInfo() {
+    $.ajax({
+        url: '/my/userinfo',
+        success: function (res) {
+            layer.msg(res.message)
+         if (res.status==0) {
+                console.log(res);
+            // 慢方法 ---  属性选择器
+            // $('input[name=username]').val(res.data.username);
+            // $('input[name=nickname]').val(res.data.nickname);
+            // $('input[name=email]').val(res.data.email);
+    
+            // LayUI 内置快速填充的方法的要求：
+            // 先设置表单各项的 name属性（username/nickname/email/id）和接口的参数名对应
+            // 为form表单设置 `lay-filter="user"` ；值随便定义，我这里使用的是 user
+            // 调用方法
+            form.val('user',res.data)
+         }
+        }
+    })
+}
+getInfo()
 
 
 // ----------------------------------------------------------更新数据
@@ -59,4 +62,12 @@ $('form').on('submit', function (e) {
             }
         }
     })
+})
+
+// ----------------------------------------------------------重置不是清空,而是恢复到服务器上数据
+$('.reset').on('click', function (e) {
+    e.preventDefault()
+
+    //重新获取
+    getInfo()
 })
